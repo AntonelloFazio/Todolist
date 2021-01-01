6e79fb5588942bb29d2862edd0957e2e8a562acb
AF <antonellof>
2021-01-01 19:06:50 +0100

    Initial Table View Setup Complete

--- Todolist/ViewController.swift
+++ Todolist/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodolistViewController: UITableViewController {
 
+    var todoArray = ["Eggs", "Milk", "Bread", "Meat"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.barTintColor = .red
+        
+        title = "To do list"
+        
+        
     }
 
+    
+    //MARK: - Table View Data source Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = todoArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - Table View Delegate Methods
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .some(.checkmark) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
+    
+  
 }
+
 
