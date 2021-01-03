9ff9f4709136194621250aa36098041e884de1b0
AF <antonellof>
2021-01-03 15:04:01 +0100

    New Item functionality complete

--- Todolist/ViewController.swift
+++ Todolist/ViewController.swift
@@ -11,13 +11,18 @@ class TodolistViewController: UITableViewController {
 
     var todoArray = ["Eggs", "Milk", "Bread", "Meat"]
     
+    let userDefault = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.barTintColor = .red
+        navigationController?.navigationBar.barTintColor = UIColor(red: 0/255, green: 250/255, blue: 154/255, alpha: 1)
         
         title = "To do list"
         
+        if let savedItems = userDefault.array(forKey: "TodoList") as? [String] {
+            todoArray = savedItems
+        }
         
     }
 
@@ -40,6 +45,7 @@ class TodolistViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        
         tableView.deselectRow(at: indexPath, animated: true)
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .some(.checkmark) {
@@ -47,10 +53,36 @@ class TodolistViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
     }
     
-  
+    @IBAction func addNewItemButtonTapped(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let ac = UIAlertController(title: "Add New Item", message: nil, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        let addAction = UIAlertAction(title: "Add Item", style: .default) { (addNewItem) in
+            
+            self.todoArray.append(textField.text!)
+            
+            self.userDefault.setValue(self.todoArray, forKey: "TodoList")
+            
+            self.tableView.reloadData()
+        }
+        
+        ac.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        ac.addAction(addAction)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
+    }
+    
+   
 }
 
 
